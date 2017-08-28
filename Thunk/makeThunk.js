a3d043caa878716b672b54cef3ae93a3654869d2
Santosh <santosharakere@gmail.com>
2017-08-28 16:29:17 +0530

    nested thunks

--- Thunk/makeThunk.js
+++ Thunk/makeThunk.js
@@ -21,15 +21,25 @@ let thunk1 = makeThunk(addAsync, 10, 12);
 // });
 
 function getData(x, cb) {
-  return cb(x * x);
+  return cb(x);
 }
 
 debugger;
 
 let get10 = makeThunk(getData, 10);
+let get30 = makeThunk(getData, 30);
 
 get10(function(d) {
-  console.log(d);
-})
+  // console.log(d);
+  let num1 = d + 1;
+  get30(function(d) {
+    let num2 = d + 1;
+    let getAnswer = makeThunk(getData, `meaing of life = ${num1 + num2}`);
+    getAnswer(function(d) {
+      debugger;
+      console.log(d);
+    });
+  });
+});
 
 
