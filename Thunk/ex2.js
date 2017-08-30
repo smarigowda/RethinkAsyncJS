012f002dd2f5984abe3d23fbae606a9019442d00
Santosh <santosharakere@gmail.com>
2017-08-30 08:15:10 +0530

    adding debugger

--- Thunk/ex2.js
+++ Thunk/ex2.js
@@ -18,14 +18,18 @@ function output(text) {
 // Active Thunk
 // There is a race condition which is resolved by using closure to maintain the state
 function getFile(file) {
+  let fileTemp = file;
   let text, fn; // using closure to maintain state
   fakeAjax(file, function(response) {
     if(fn) {
+      // console.log(file);
+      debugger;
       // if the thunk is run, then call the callback
       fn(response);
     } else {
       // if the thunk is not yet run, then
       // hold on to the response
+      debugger;
       text = response; 
     }
   })
@@ -33,6 +37,7 @@ function getFile(file) {
   return function(cb) {
     if(text) {
       // response is ready, call the callback
+      debugger;
       cb(text);
     } else {
       // response is not yet ready
@@ -42,11 +47,13 @@ function getFile(file) {
   }
 }
 
-
-
+debugger;
 let th1 = getFile('file1');
+debugger;
 let th2 = getFile('file2');
+debugger;
 let th3 = getFile('file3');
+debugger;
 
 // thunks do not solve the callback issue
 // eliminate time complexity
@@ -54,10 +61,13 @@ let th3 = getFile('file3');
 // no looping through array // massive improvement
 // just using functions in different way
 th1(function(text1) {
+  debugger;
   output(text1);
   th2(function(text2) {
+    debugger;
     output(text2);
     th3(function(text3) {
+      debugger;
       output(text3);
       console.log('Complete!');
     })
